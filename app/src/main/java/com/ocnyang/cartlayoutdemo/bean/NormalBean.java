e8911e4a208057251ab94c21a5a075498be35515
ZYF <63950267+ZYFnp@users.noreply.github.com>
2020-06-10 23:17:49 +0800

    细节修改

--- app/src/main/java/com/ocnyang/cartlayoutdemo/bean/NormalBean.java
+++ app/src/main/java/com/ocnyang/cartlayoutdemo/bean/NormalBean.java
@@ -2,13 +2,6 @@ package com.ocnyang.cartlayoutdemo.bean;
 
 import com.ocnyang.cartlayout.bean.CartItemBean;
 
-/*******************************************************************
- *    * * * *   * * * *   *     *       Created by OCN.Yang
- *    *     *   *         * *   *       Time: 2018/6/23 14:03.
- *    *     *   *         *   * *       Email address: ocnyang@gmail.com
- *    * * * *   * * * *   *     *.Yang  Web site: www.ocnyang.com
- *******************************************************************/
-
 public class NormalBean extends CartItemBean{
     int markdownNumber;
 
