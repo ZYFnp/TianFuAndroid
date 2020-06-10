868e6769e72799b5ef79de81d51980988c226ea2
ZYF <63950267+ZYFnp@users.noreply.github.com>
2020-06-10 19:53:03 +0800

    增加个人中心返回主页跳转

--- app/src/main/java/com/ocnyang/cartlayoutdemo/person.java
+++ app/src/main/java/com/ocnyang/cartlayoutdemo/person.java
@@ -1,13 +1,22 @@
 package com.ocnyang.cartlayoutdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class person extends AppCompatActivity {
+
+    private ImageView houtuo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
+        houtuo=findViewById(R.id.houtuo);
+        houtuo.setOnClickListener(v->{
+            Intent intent=new Intent(this,Home.class);
+            startActivity(intent);
+        });
     }
 }
