98be82e02391b43320efa258a4e9edddb21de403
ZYF <63950267+ZYFnp@users.noreply.github.com>
2020-06-10 13:03:30 +0800

    增加主页到购物车界面的跳转功能

--- app/src/main/java/com/ocnyang/cartlayoutdemo/Home.java
+++ app/src/main/java/com/ocnyang/cartlayoutdemo/Home.java
@@ -1,13 +1,27 @@
 package com.ocnyang.cartlayoutdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
+
+    private ImageView imageView8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        imageView8=findViewById(R.id.imageView8);
+        imageView8.setOnClickListener(v->{
+            Intent intent=new Intent(this,MainActivity.class);
+            startActivity(intent);
+        });
+
+
     }
+
+
 }
